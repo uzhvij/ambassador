bea4c56175b4f0b5a04d4b5b7371454443a84635
Alexander Bondarenko <uzhvij@gmail.com>
2019-08-16 19:57:37 +0300

    work at the registration

---
+++ app/src/main/java/com/blogspot/uzhvij/pr_ambassador/api_classes/RegistrationBody.java
@@ -0,0 +1,77 @@
+package com.blogspot.uzhvij.pr_ambassador.api_classes;
+
+public class RegistrationBody {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String phone;
+    private String lat;
+    private String lng;
+    private String imei;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+}
